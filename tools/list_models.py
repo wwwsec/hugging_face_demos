1ab63b0282c8b1695ab2c4149c107fee865950b6
wwwsec <yuecaili@gmail.com>
2024-11-20 17:08:08 +0800

    .

--- tools/list_models.py
+++ tools/list_models.py
@@ -4,7 +4,8 @@ from huggingface_hub import HfApi
 api = HfApi()
 
 # 搜索英文到中文的翻译模型
-models = api.list_models(filter="translation", search="en-zh")
+#models = api.list_models(filter="audio-classification", search="en-zh")
+models = api.list_models(filter="audio-classification")
 
 # 打印模型名称
 for model in models:
