b204e74b2a3d35adbffd97d92de0fb2d19e54a1b
wwwsec <yuecaili@gmail.com>
2024-11-20 11:11:08 +0800

    .

---
+++ text_to_image/stabilityai_stable-diffusion-2.py
@@ -0,0 +1,13 @@
+from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
+import torch
+from PIL import Image
+
+model_id = "stabilityai/stable-diffusion-2"
+scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
+pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler, torch_dtype=torch.float16)
+pipe = pipe.to("cuda")
+
+prompt = "a photo of an astronaut riding a horse on mars"
+image = pipe(prompt).images[0]
+
+image.save("generated_image.png")
